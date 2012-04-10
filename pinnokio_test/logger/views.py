6f4fe6c76d7b10c99ee4d89ebf5a14420939ca8d
Konstantin Lazarenko <pinnokio@gmail.com>
2012-04-10 12:14:04 +0300

    ticket:3 index.html, 404.html created. Ticket implemented

--- pinnokio_test/logger/views.py
+++ pinnokio_test/logger/views.py
@@ -1,1 +1,9 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+
+from pinnokio_test.logger.models import RequestEntry
+
+
+def index(request):
+    requests = RequestEntry.objects.all().order_by('creation_time')[:10]
+    return render_to_response('logger/index.html',
+            {'requests': requests})
--- pinnokio_test/urls.py
+++ pinnokio_test/urls.py
@@ -17,6 +17,7 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('pinnokio_test.contact.urls')),
+    url(r'^logger/', include('pinnokio_test.logger.urls')),
 )
 
 if settings.DEBUG:
