9e9dae97fdd02ee36060517fb92623a06dd91cd8
Konstantin Lazarenko <pinnokio@gmail.com>
2012-04-13 12:32:38 +0300

    ticket:4 Moved context app into extra. All code are commented for failing test

--- pinnokio_test/context/context_processors.py
+++
@@ -1,9 +0,0 @@
-#-*- coding: utf8 -*-
-
-from django.conf import settings
-
-
-def settings_context_processor(request):
-    """ Processor to add django settings into context """
-
-    return {'settings': settings}
