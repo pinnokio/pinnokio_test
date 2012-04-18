198cb17ecf3dd65f73c0b6c693f55d2e11a238ee
Konstantin Lazarenko <pinnokio@gmail.com>
2012-04-18 16:09:50 +0300

    ticket:5 Logout/login action implemented. Context added to contact details view (index)  in views.py for properly display login/logout links. Login .html created.

--- pinnokio_test/contact/views.py
+++ pinnokio_test/contact/views.py
@@ -1,8 +1,27 @@
-from django.shortcuts import render_to_response, get_object_or_404
+from django.shortcuts import render_to_response, redirect, get_object_or_404
+from django.core.urlresolvers import reverse
+from django.template.context import RequestContext
 
 from pinnokio_test.contact.models import Person
+from pinnokio_test.contact.forms import PersonEditForm
 
 
 def index(request):
     my_person = get_object_or_404(Person, pk=1)
-    return render_to_response('contact/index.html', {'person': my_person})
+    return render_to_response('contact/index.html',
+                              {'person': my_person},
+                              context_instance=RequestContext(request))
+
+def contact_edit(request):
+    home_url = reverse('index')
+    my_person = get_object_or_404(Person, pk=1)
+    form = PersonEditForm(request.POST or None,
+                          request.FILES or None,
+                          instance=person)
+    if form.is_valid():
+        form.save()
+        return redirect('index')
+    return render_to_response('contact/edit.html',
+                              {'form': form,
+                               'next': request.GET.get('next', home_url)},
+                               context_instance=RequestContext(request))
